0cdb67e0ee07d1e2ddcb75eb8d19746e61bfa186
Evgeny Kryukov <krukov@bpcbt.com>
2017-05-22 22:55:51 +0300

    fixup

--- fabfile.py
+++ fabfile.py
@@ -95,7 +95,7 @@ def collect_static():
     require('environment', provided_by=[production1, production2])  # дописать по желанию dev и stage
     print(green('Collect static'))
     with virtualenv():
-        run("python manage.py collecstatic")
+        run("python manage.py collectstatic")
 
 
 def stop_webserver():
