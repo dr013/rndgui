972e4ad6223cd22874e71024c01fabc8e5db151e
Evgeny Kryukov <krukov@bpcbt.com>
2017-05-22 18:12:39 +0300

    New login form

--- acm/forms.py
+++ acm/forms.py
@@ -4,5 +4,3 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label='BPC Login', max_length=20, required=True)
     password = forms.CharField(label='BPC Password', required=True, widget=forms.PasswordInput)
-    is_ldap = forms.BooleanField(widget=forms.HiddenInput, required=True, initial=True)
-
