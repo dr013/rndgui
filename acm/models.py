b4cfb017f61dc1772100eabfb623877e73f114da
Evgeny Kryukov <krukov@bpcbt.com>
2017-05-25 11:27:50 +0300

    Format product list

--- acm/models.py
+++ acm/models.py
@@ -6,18 +6,17 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-# Create your models here.
 class Institution(models.Model):
     inst_name = models.CharField(_("Group name"), max_length=200)
     slug_name = models.SlugField()
-    user = models.ManyToManyField(User, through='Membership')
+    user = models.ManyToManyField(User, through=_('Membership'))
 
     def __str__(self):  # __unicode__ on Python 2
         return self.inst_name
 
     class Meta:
-        verbose_name = u'Group'
-        verbose_name_plural = u'Groups'
+        verbose_name = _('Group')
+        verbose_name_plural = _('Groups')
 
 
 class Membership(models.Model):
