c181ecf445d49ae1ad658d12ec0d2f102e991a39
Dorontcov Sergey <dorontcov@bpc.in>
2017-06-01 09:36:59 +0300

    Develop InstanceUI

--- cat/models.py
+++ cat/models.py
@@ -4,18 +4,23 @@ from __future__ import unicode_literals
 from django.db import models
 from django.urls import reverse
 
+WEIGHT_ARR = (
+    ('Big', 'For SmartVista project'),
+    ('Medium', 'For SMSGate, SSO projects'),
+    ('Small', 'For other project'),
+)
+
 
 # Create your models here.
 class DBInstance(models.Model):
     host = models.CharField('Host', max_length=200)
-    sid = models.CharField('SID', max_length=20, blank=True, null=True)
+    sid = models.CharField('SID', max_length=20, blank=True, null=True, default='SV')
     port = models.IntegerField('Port', default=1521)
     login = models.CharField('Login', max_length=200)
     passwd = models.CharField('Password', max_length=200)
-    dbname = models.CharField('Service', max_length=20, blank=True, null=True)
     sys_user = models.CharField('System Login', max_length=200, blank=True, null=True, default='system')
     sys_passwd = models.CharField('System Password', max_length=200, blank=True, null=True, default='SYSTEM1')
-    weight = models.IntegerField('Weight')
+    weight = models.CharField('Weight', choices=WEIGHT_ARR, max_length=200,)
 
     def __str__(self):
         return '{host}::{login}'.format(host=self.host, login=self.login)
--- cat/views.py
+++ cat/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from .models import *
-
+from django.contrib import messages
 # Create your views here.
 
 
@@ -18,13 +18,24 @@ class InstanceDetail(DetailView):
 
 class CreateInstance(CreateView):
     model = DBInstance
-    fields = ['host', 'sid', 'port', 'login', 'passwd', 'dbname', 'sys_user', 'sys_passwd', 'weight']
+    fields = ['host', 'sid', 'port', 'login', 'passwd', 'sys_user', 'sys_passwd', 'weight']
 
 
 class UpdateInstance(UpdateView):
     model = DBInstance
-    fields = ['host', 'sid', 'port', 'login', 'passwd', 'dbname', 'sys_user', 'sys_passwd', 'weight']
+    fields = ['host', 'sid', 'port', 'login', 'passwd', 'sys_user', 'sys_passwd', 'weight']
+    success_message = 'Instance was updated successfully.'
+
+    def form_valid(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(UpdateInstance, self).form_valid(request, *args, **kwargs)
 
 
 class DeleteInstance(DeleteView):
     model = DBInstance
+    success_url = reverse_lazy('instance-list')
+    success_message = 'Instance was deleted successfully.'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(DeleteInstance, self).delete(request, *args, **kwargs)
