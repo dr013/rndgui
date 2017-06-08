df3dc33d87a036642cf2fa536c49332c8c331e9c
Evgeny Kryukov <krukov@bpcbt.com>
2017-06-08 10:53:06 +0300

    gunicorn config was added

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,10 @@
+import multiprocessing
+
 # bind = "127.0.0.1:8888"
 
-bind = "unix:/home/proft/projects/blog/run/blog.socket"
-workers = 5
+bind = "unix:/srv/rndgui/rndgui.sock"
+# worker numbers 2xCPUs + 1
+workers = multiprocessing.cpu_count() * 2 + 1
 user = "uwsgi"
 group = "uwsgi"
 logfile = "/srv/rndgui/logs/gunicorn.log"
