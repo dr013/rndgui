4e9417796822ad84b3226921d788aae2ba40159a
Evgeny Kryukov <krukov@bpcbt.com>
2017-06-08 16:27:35 +0300

    Gunicorn - added log

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,7 +7,9 @@ bind = "unix:/srv/rndgui/rndgui.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
 user = "uwsgi"
 group = "uwsgi"
-logfile = "/srv/rndgui/logs/gunicorn.log"
+errorlog = "/srv/rndgui/logs/error-gunicorn.log"
 loglevel = "info"
+
 proc_name = "rndgui"
-# pidfile = "/home/proft/projects/blog/gunicorn.pid"
+accesslog = "/srv/rndgui/logs/access-gunicorn.log"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
