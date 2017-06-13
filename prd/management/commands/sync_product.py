d10affa460c2b37203070e7d40ef00ef80732899
Evgeny Kryukov <krukov@bpcbt.com>
2017-06-13 20:31:03 +0300

    Added clean_pyc command in fabfile Sync product - remove hardcode Bump version to 1.6.29

--- prd/management/commands/sync_product.py
+++ prd/management/commands/sync_product.py
@@ -4,28 +4,28 @@ from django.contrib.auth.models import User
 from prd.models import Product, Build, Release, BuildRevision, ReleasePart
 
 
-def sync_build(release):
+def sync_build(release, prod_jira):
     client = MongoClient('mongodb://sv2.bpc.in:27017')
     db = client['ci']
     product = 'sv'
     author = User.objects.get(pk=1)
-    release_obj = Release.objects.get(pk=3)
+    release_obj = Release.objects.get(name=release, product__jira=prod_jira)
     curs = db.release_list.find({'product': product, 'release': release, 'date_release': {'$ne': None}})\
         .sort('date_release')
     for rec in curs:
-        print rec['release'], rec['build']
+        print (rec['release'], rec['build'])
         build, created = Build.objects.get_or_create(release=release_obj, name=rec['build'], author=author)
         build.released = True
         build.author = author
         build.date_released = rec['date_release']
-        for rec2 in ReleasePart.objects.filter(product__pk=1):
+        for rec2 in ReleasePart.objects.filter(product=release_obj.product):
             for rec3 in rec['component']:
                 if rec3['name'] == rec2.name:
                     revision = BuildRevision(build=build, release_part=rec2, revision=rec3['revision'])
                     revision.save()
 
         build.save()
-        print build, created
+        print (build, created)
 
 
 class Command(BaseCommand):
@@ -41,7 +41,7 @@ class Command(BaseCommand):
         try:
             product = Product.objects.get(jira=jira)
             self.stdout.write('Product found {}'.format(product.title))
-            sync_build(release)
+            sync_build(release, jira)
 
         except Product.DoesNotExist:
             raise CommandError('Product "%s" does not exist' % jira)
